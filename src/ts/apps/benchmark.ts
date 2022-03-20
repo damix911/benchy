b3b53e9ddbf318788b890fc4be57e1e1e03eece4
Dario D'Amico <ddamico@esri.com>
2022-03-19 20:51:24 -0700

    Give up and adopt webpack.

--- src/ts/apps/benchmark.ts
+++ src/ts/apps/benchmark.ts
@@ -2,6 +2,8 @@ import myFirstTest from "../tests/myFirstTest";
 import mySecondTest from "../tests/mySecondTest";
 import { runTests } from "../util";
 
+console.log("benchmark!");
+
 runTests([
   myFirstTest,
   mySecondTest
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  entry: {
+    benchmark: "./src/ts/apps/benchmark.ts",
+    results: "./src/ts/apps/results.ts"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, "src", "html"), to: path.resolve(__dirname, "dist") }
+      ],
+    }),
+  ]
+};
